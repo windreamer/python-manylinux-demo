8f218f2cc46a872683f12be3587139900eaa03a9
tianzhongbo <tianzhongbo@douban.com>
2016-11-18 15:28:55 +0800

    add pypy dir

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,6 +4,10 @@ set -e -x
 # Install a system package required by our library
 yum install -y atlas-devel
 
+mkdir -p /opt/python/pypy /opt/python/pypy3
+wget "https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-5.6-linux_$(uname -m)-portable.tar.bz2" -qO - | tar xj -C /opt/python/pypy/
+wget "https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.3-5.5-alpha-20161013-linux_i$(uname -m)-portable.tar.bz2" -qO - | tar xj -C /opt/python/pypy3/
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     ${PYBIN}/pip install -r /io/dev-requirements.txt
@@ -18,5 +22,5 @@ done
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
     ${PYBIN}/pip install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
+    (cd /io; ${PYBIN}/nosetests pymanylinuxdemo)
 done
